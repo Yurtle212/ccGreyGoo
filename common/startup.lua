84eefff4aec2b84bb1ecd3198b00d9fe505e0f44
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-10-02 13:51:59 -0700

    first commit

---
+++ common/startup.lua
@@ -0,0 +1,15 @@
+Headings = {
+    north = 0,
+    east = 1,
+    south = 2,
+    west = 3,
+}
+
+settings.define("heading", {
+    description = "Heading of the turtle",
+    default = Headings.north,
+    type = number,
+})
+
+shell.run("delete json")
+shell.run("wget https://pastebin.com/raw/4nRg9CHU json")
---
+++ common/util.lua
@@ -0,0 +1,55 @@
+local fuelItems = {
+    ["minecraft:coal_block"] = 800,
+    ["minecraft:dried_kelp_block"] = 200,
+    ["minecraft:coal"] = 80,
+}
+
+local NUM_SLOTS = 16
+
+local function findItemInInventory(itemName)
+    for i = 1, NUM_SLOTS, 1 do
+        local item = turtle.getItemDetail(i)
+        if (item ~= nil) then
+            if (itemName == "fuel") then
+                for key, value in pairs(fuelItems) do
+                    if key == item.name then
+                        return i
+                    end
+                end
+            elseif (item.name) == itemName then
+                return i
+            end
+        elseif itemName == "empty" then
+            return i
+        end
+    end
+    return nil
+end
+
+local function refuel(amount)
+    if (amount == nil) then
+        amount = turtle.getFuelLimit() - turtle.getFuelLevel()
+    end
+    local fueledAmount = 0
+
+    while fueledAmount < amount do
+        local slot = findItemInInventory("fuel")
+        if (slot == nil) then
+            break
+        end
+
+        turtle.select(slot)
+        local detail = turtle.getItemDetail(slot)
+        if (fuelItems[detail.name] * detail.count) > amount then
+            while fueledAmount < amount do
+                turtle.refuel(1)
+                fueledAmount = fueledAmount + fuelItems[detail.name] * detail.count
+            end
+        else
+            turtle.refuel()
+            fueledAmount = fueledAmount + fuelItems[detail.name] * detail.count
+        end
+    end
+end
+
+return { refuel = refuel, findItemInInventory = findItemInInventory, fuelItems = fuelItems, NUM_SLOTS = 16 }
