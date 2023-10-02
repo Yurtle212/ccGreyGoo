20487dacaedcf17f77342da97587370c16d5a1f8
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-10-02 13:54:49 -0700

    self updating

--- common/startup.lua
+++ common/startup.lua
@@ -15,4 +15,4 @@ shell.run("delete json")
 shell.run("wget https://pastebin.com/raw/4nRg9CHU json")
 
 shell.run("delete startup.lua")
-shell.run("wget https://raw.githubusercontent.com/Yurtle212/ccGreyGoo/main/common/startup.lua")
+shell.run("wget https://raw.githubusercontent.com/Yurtle212/ccGreyGoo/main/common/startup.lua?t=" .. os.time())
