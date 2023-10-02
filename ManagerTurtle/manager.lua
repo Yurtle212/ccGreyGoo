67e6903056006f74bf07692c1ebd2fbe45081be9
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-10-02 15:18:03 -0700

    added sleep to manager

--- ManagerTurtle/manager.lua
+++ ManagerTurtle/manager.lua
@@ -2,7 +2,9 @@ local movement = require "movement"
 local ws = require "communication"
 
 local function main()
-    
+    while true do
+        sleep(1)
+    end
 end
 
 parallel.waitForAny(main, ws.websocketHandler)
