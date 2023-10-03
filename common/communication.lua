9d338bea0a07c7b3b98f8421d174459cfca01e28
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-10-02 17:28:54 -0700

    added check for closed websocket

--- common/communication.lua
+++ common/communication.lua
@@ -1,22 +1,25 @@
 -- os.loadAPI("json")
 
 Websocket = assert(http.websocket("wss://yurtle.net/cc/" .. settings.get("wsid")))
+WebsocketOpen = false
 
 local function websocketHandler()
     while true do
         local raw = Websocket.receive()
         if (raw == nil) then
             print("re-init websocket")
+            WebsocketOpen = false
             os.startTimer(2)
             os.pullEvent("timer")
 
             Websocket = assert(http.websocket("wss://yurtle.net/cc/" .. settings.get("wsid")))
+            WebsocketOpen = true
         elseif raw ~= nil then
             local signal = textutils.unserialiseJSON(raw)
             if signal.type == "reboot" then
                 shell.run("delete startup.lua")
                 shell.run("wget https://raw.githubusercontent.com/Yurtle212/ccGreyGoo/main/common/startup.lua?t=" ..
-                os.time())
+                    os.time())
 
                 shell.run("reboot")
             end
@@ -25,6 +28,9 @@ local function websocketHandler()
 end
 
 local function sendSignal(signalType, data)
+    while ~WebsocketOpen do
+        sleep(1)
+    end
     Websocket.send(textutils.serialiseJSON({
         type = signalType,
         data = data,
